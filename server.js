06c507338cc124299e82bf2a87fbb8379d44f200
MohamadAlGhorani <moh.gh266@gmail.com>
2020-03-10 13:06:21 +0100

    clean js server.js

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require("express");
+const moviesRouter = require("./routes/moviesRoute");
 
 const config = {
     port: 3000
 };
 
 const app = express();
-app.set("view engine", "ejs");
-app.set("views", "views");
 
-app.use(express.static("public"));
+app
+    .set("view engine", "ejs")
+    .set("views", "views")
 
-var moviesRouter = require("./routes/moviesRoute");
-app.use("/movies", moviesRouter);
+    .use(express.static("public"))
+    .use("/movies", moviesRouter)
 
-app.listen(config.port, function () {
-    console.log(`Application started on port: ${config.port}`);
-});
+    .listen(config.port, function () {
+        console.log(`Application started on port: ${config.port}`);
+    });

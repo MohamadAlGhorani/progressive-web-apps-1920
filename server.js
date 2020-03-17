46b7fae5b5270537656aeb76fe9407e34e2a090e
MohamadAlGhorani <moh.gh266@gmail.com>
2020-03-17 13:01:07 +0100

    movies als default maken

--- server.js
+++ server.js
@@ -2,19 +2,22 @@ const express = require("express");
 const moviesRouter = require("./routes/moviesRoute");
 
 const config = {
-    port: process.env.PORT || 3000
+  port: process.env.PORT || 3000
 };
 
 const app = express();
 
+(function() {
+  window.location = "/movies";
+})();
+
 app
-    .set("view engine", "ejs")
-    .set("views", "views")
+  .set("view engine", "ejs")
+  .set("views", "views")
 
-    .use(express.static('static'))
-    .use("/movies", moviesRouter)
+  .use(express.static("static"))
+  .use("/movies", moviesRouter)
 
-
-    .listen(config.port, function () {
-        console.log(`Application started on port: ${config.port}`);
-    });
+  .listen(config.port, function() {
+    console.log(`Application started on port: ${config.port}`);
+  });

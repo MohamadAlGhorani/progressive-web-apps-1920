3ac3f367b1b0b38aadf7f37c621d41719b4dc8dd
MohamadAlGhorani <moh.gh266@gmail.com>
2020-03-17 16:33:11 +0100

    service worker and manifest.json

--- routes/moviesRoute.js
+++ routes/moviesRoute.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const router = express.Router();
-require('dotenv').config()
+require("dotenv").config();
 
-router.get("/", function (req, res) {
+router.get("/", function(req, res) {
   Promise.all([
     fetch(
       `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${process.env.movieDbKey}`
@@ -30,43 +30,51 @@ router.get("/", function (req, res) {
   });
 });
 
-router.get("/genres", function (req, res) {
-  fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.movieDbKey}`)
-    .then(async response => {
-      const genresData = await response.json()
-      res.render('filter', {
-        title: 'Genres',
-        genresData
-      });
-    })
+router.get("/offline", function(req, res) {
+  res.render("offline", {
+    title: "offline"
+  });
 });
 
-router.get("/search", function (req, res) {
-  fetch(`https://api.themoviedb.org/3/search/movie?query=${req.query.query}&api_key=${process.env.movieDbKey}`)
-    .then(async response => {
-      const moviesData = await response.json()
-      res.render('results', {
-        title: 'Movies',
-        query: req.query.query,
-        moviesData: moviesData.results
-      });
-    })
+router.get("/genres", function(req, res) {
+  fetch(
+    `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.movieDbKey}`
+  ).then(async response => {
+    const genresData = await response.json();
+    res.render("filter", {
+      title: "Genres",
+      genresData
+    });
+  });
 });
 
-
-router.get("/genres/:name/:id", function (req, res) {
-  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.movieDbKey}&sort_by=popularity.desc&with_genres=${req.params.id}`)
-    .then(async response => {
-      const moviesData = await response.json()
-      res.render('filterResults', {
-        title: `Genere ${req.params.id}`,
-        moviesData: moviesData.results,
-        genreName: req.params.name
-      });
-    })
+router.get("/search", function(req, res) {
+  fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${req.query.query}&api_key=${process.env.movieDbKey}`
+  ).then(async response => {
+    const moviesData = await response.json();
+    res.render("results", {
+      title: "Movies",
+      query: req.query.query,
+      moviesData: moviesData.results
+    });
+  });
 });
 
-router.get("/:id", function (req, res) {
+router.get("/genres/:name/:id", function(req, res) {
+  fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.movieDbKey}&sort_by=popularity.desc&with_genres=${req.params.id}`
+  ).then(async response => {
+    const moviesData = await response.json();
+    res.render("filterResults", {
+      title: `Genere ${req.params.id}`,
+      moviesData: moviesData.results,
+      genreName: req.params.name
+    });
+  });
+});
+
+router.get("/:id", function(req, res) {
   Promise.all([
     fetch(
       `https://api.themoviedb.org/3/movie/${req.params.id}?api_key=${process.env.movieDbKey}`

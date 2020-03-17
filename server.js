73e663725e835254cc26ffaaf2554115dce366cc
MohamadAlGhorani <moh.gh266@gmail.com>
2020-03-17 13:18:56 +0100

    test

--- server.js
+++ server.js
@@ -2,24 +2,23 @@ const express = require("express");
 const moviesRouter = require("./routes/moviesRoute");
 
 const config = {
-  port: process.env.PORT || 3000
+    port: process.env.PORT || 3000
 };
 
 const app = express();
 
-app.get("/", function(req, res) {
-  // Send a plain string using res.send();
-  console.log("hii");
-  window.location = "/movies";
+app.get("/", function (req, res) {
+    // Send a plain string using res.send();
+    res.redirect('/movies')
 });
 
 app
-  .set("view engine", "ejs")
-  .set("views", "views")
+    .set("view engine", "ejs")
+    .set("views", "views")
 
-  .use(express.static("static"))
-  .use("/movies", moviesRouter)
+    .use(express.static("static"))
+    .use("/movies", moviesRouter)
 
-  .listen(config.port, function() {
-    console.log(`Application started on port: ${config.port}`);
-  });
+    .listen(config.port, function () {
+        console.log(`Application started on port: ${config.port}`);
+    });

52e6e1c421646f1aa8bedb74061b67473ae62269
MohamadAlGhorani <moh.gh266@gmail.com>
2020-03-23 18:24:36 +0100

    fix mobile

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,4 +1,4 @@
-const CORE_CACHE_VERSION = "v7";
+const CORE_CACHE_VERSION = "v8";
 const CORE_ASSETS = ["/movies/offline", "/index.css", "/index.js"];
 
 self.addEventListener("install", event => {

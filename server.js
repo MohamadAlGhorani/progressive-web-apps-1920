937d82836517ef33c23d4904f5ac43c2601f1deb
MohamadAlGhorani <moh.gh266@gmail.com>
2020-03-23 17:12:49 +0100

    using compression

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
 const express = require("express");
+const compression = require("compression");
 const moviesRouter = require("./routes/moviesRoute");
 
 const config = {
-    port: process.env.PORT || 3000
+  port: process.env.PORT || 3000
 };
 
 const app = express();
 
-app.get("/", function (req, res) {
-    // Send a plain string using res.send();
-    res.redirect('/movies')
+app.get("/", function(req, res) {
+  // Send a plain string using res.send();
+  res.redirect("/movies");
 });
 
 app
-    .enable('etag') // use strong etags
-    .set('etag', 'strong') // same
-    .set("view engine", "ejs")
-    .set("views", "views")
+  .enable("etag") // use strong etags
+  .set("etag", "strong") // same
+  .set("view engine", "ejs")
+  .set("views", "views")
 
-    .use((req, res, next) => {
-        res.header('Cache-Control', 'max-age=2592000000');
-        next();
-    })
-    .use(express.static("static"))
-    .use("/movies", moviesRouter)
+  .use(compression())
+  .use((req, res, next) => {
+    res.header("Cache-Control", "max-age=2592000000");
+    next();
+  })
+  .use(express.static("static"))
+  .use("/movies", moviesRouter)
 
-    .listen(config.port, function () {
-        console.log(`Application started on port: ${config.port}`);
-    });
+  .listen(config.port, function() {
+    console.log(`Application started on port: ${config.port}`);
+  });
